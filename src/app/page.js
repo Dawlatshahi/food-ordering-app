4455d07f6dc87b80d0bc9cb465c697a7db90a97c
Farzana Asadi <121952162+Dawlatshahi@users.noreply.github.com>
2024-07-30 19:04:09 +0300

    Register page layout done.

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ import SectionHeaders from '@/components/layout/SectionHeaders';
 export default function Home() {
 	return (
 		<>
-			<Header />
 			<Hero />
 			<HomeMenu />
 
@@ -15,7 +14,7 @@ export default function Home() {
 				<div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
 					<p>
 						Lorem Ipsum is simply dummy text of the printing and typesetting
-						industry. Lorem Ipsum has been the industry's standard dummy text
+						industry. Lorem Ipsum has been the industrys standard dummy text
 						ever since the 1500s, when an unknown printer took a galley of type
 						and scrambled it to make a type specimen book. It has survived not
 						only five centuries, but also the leap into electronic typesetting,
@@ -23,7 +22,7 @@ export default function Home() {
 					</p>
 					<p>
 						Lorem Ipsum is simply dummy text of the printing and typesetting
-						industry. Lorem Ipsum has been the industry's standard dummy text
+						industry. Lorem Ipsum has been the industrys standard dummy text
 						ever since the 1500s, when an unknown printer took a galley of type
 						and scrambled it to make a type specimen book. It has survived not
 						only five centuries, but also the leap into electronic typesetting,
@@ -31,7 +30,7 @@ export default function Home() {
 
 					<p>
 						Lorem Ipsum is simply dummy text of the printing and typesetting
-						industry. Lorem Ipsum has been the industry's standard dummy text
+						industry. Lorem Ipsum has been the industrys standard dummy text
 					</p>
 				</div>
 			</section>
@@ -49,10 +48,6 @@ export default function Home() {
 					</a>
 				</div>
 			</section>
-
-			<footer className="border-t p-8 text-center text-gray-500 mt-16">
-				&copy; 2024 All rights reserved
-			</footer>
 		</>
 	);
 }
