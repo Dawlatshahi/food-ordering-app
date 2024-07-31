5024d7ed22484f7ec58b96ec6590f15163e10827
Farzana Asadi <121952162+Dawlatshahi@users.noreply.github.com>
2024-07-31 20:37:14 +0300

    Login, Logout, cart and mobile menu done.

--- src/app/page.js
+++ src/app/page.js
@@ -1,41 +1,34 @@
+import Header from '@/components/layout/Header';
 import Hero from '@/components/layout/Hero';
 import HomeMenu from '@/components/layout/HomeMenu';
 import SectionHeaders from '@/components/layout/SectionHeaders';
 
 export default function Home() {
-	console.log('Home component rendered');
-
 	return (
 		<>
 			<Hero />
 			<HomeMenu />
-
-			<section className="text-center my-16">
+			<section className="text-center my-16" id="about">
 				<SectionHeaders subHeader={'Our story'} mainHeader={'About us'} />
 				<div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
 					<p>
-						Lorem Ipsum is simply dummy text of the printing and typesetting
-						industry. Lorem Ipsum has been the industrys standard dummy text
-						ever since the 1500s, when an unknown printer took a galley of type
-						and scrambled it to make a type specimen book. It has survived not
-						only five centuries, but also the leap into electronic typesetting,
-						remaining essentially unchanged. It was popularised in the 1960s
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni
+						minima odit recusandae. Illum ipsa non repudiandae? Eum ipsam iste
+						quos suscipit tempora? Aperiam esse fugiat inventore laboriosam
+						officiis quam rem!
 					</p>
 					<p>
-						Lorem Ipsum is simply dummy text of the printing and typesetting
-						industry. Lorem Ipsum has been the industrys standard dummy text
-						ever since the 1500s, when an unknown printer took a galley of type
-						and scrambled it to make a type specimen book. It has survived not
-						only five centuries, but also the leap into electronic typesetting,
+						At consectetur delectus ducimus est facere iure molestias obcaecati
+						quaerat vitae voluptate? Aspernatur dolor explicabo iste minus
+						molestiae pariatur provident quibusdam saepe?
 					</p>
-
 					<p>
-						Lorem Ipsum is simply dummy text of the printing and typesetting
-						industry. Lorem Ipsum has been the industrys standard dummy text
+						Laborum molestias neque nulla obcaecati odio quia quod reprehenderit
+						sit vitae voluptates? Eos, tenetur.
 					</p>
 				</div>
 			</section>
-			<section className="text-center my-8">
+			<section className="text-center my-8" id="contact">
 				<SectionHeaders
 					subHeader={"Don't hesitate"}
 					mainHeader={'Contact us'}
@@ -45,7 +38,7 @@ export default function Home() {
 						className="text-4xl underline text-gray-500"
 						href="tel:+905338383872"
 					>
-						+90 533 838 3872
+						+90 533 8383872
 					</a>
 				</div>
 			</section>
